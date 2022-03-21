c1e5fe5a4488db4b32a1d9b5e88736a522752946
Guillaume Bernier <guillaume45Bernier@gmail.com>
2022-03-21 16:06:20 +0100

    maj du main

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,18 +3,33 @@ package main;
 import controleur.*;
 import vue.*;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         int jeu=Ihm.selectionJeu();
+        int nbJoueur = Ihm.ordiOuJoueur() ;
         Controleur controleur;
         Ihm ihm;
         if (jeu == 1){
-            ihm=new IhmNim();
-            controleur=new ControleurNim(ihm);
+            if (nbJoueur == 2) {
+                ihm = new IhmNim();
+                controleur = new ControleurNim(ihm);
+            }
+            else { //appeler d'autres ihm et contrôleur spécifique pour le jeu contre l'IA
+                ihm = new IhmNim();
+                controleur = new ControleurNim(ihm);
+            }
         }
         else {
-            ihm=new IhmPuissance();
-            controleur=new ControleurPuissance(ihm);
+            if (nbJoueur == 2){
+                ihm=new IhmPuissance();
+                controleur=new ControleurPuissance(ihm);
+            }
+            else{ //appeler d'autres ihm et contrôleur spécifique pour le jeu contre l'IA
+                ihm=new IhmPuissance();
+                controleur=new ControleurPuissance(ihm);
+            }
         }
         controleur.jouer();
     }
