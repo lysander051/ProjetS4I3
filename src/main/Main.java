cadbc011ce025666a4e596d416fed7df16a8e0a5
Badet Maxime <badet.maxime@gmail.com>
2022-03-25 12:25:20 +0100

    olerobot implementation de base

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,41 +3,19 @@ package main;
 import controleur.*;
 import vue.*;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         int jeu=Ihm.selectionJeu();
-        int nbJoueur = Ihm.ordiOuJoueur() ;
         Controleur controleur;
         Ihm ihm;
         if (jeu == 1){
-            if (nbJoueur == 2) {
-                ihm = new IhmNim();
-                controleur = new ControleurNim(ihm);
-            }
-            else { //appeler d'autres ihm et contrôleur spécifique pour le jeu contre l'IA
-                ihm = new IhmNim();
-                controleur = new ControleurNim(ihm);
-            }
+            ihm=new IhmNim();
+            controleur=new ControleurNim(ihm);
         }
         else {
-            if (nbJoueur == 2){
-                ihm=new IhmPuissance();
-                controleur=new ControleurPuissance(ihm);
-            }
-            else{ //appeler d'autres ihm et contrôleur spécifique pour le jeu contre l'IA
-                ihm=new IhmPuissance();
-                controleur=new ControleurPuissance(ihm);
-            }
+            ihm=new IhmPuissance();
+            controleur=new ControleurPuissance(ihm);
         }
         controleur.jouer();
-
-
-        boolean a=true;
-        Boolean.valueOf(a);
-
     }
-
-
 }
